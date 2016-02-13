4975a122a94cecd3a04eb8e91ca795e875ed7817
STDNT <STDNT>
2016-02-13 15:50:36 +0300

    first commit

---
+++ app/src/main/java/com/judydimpoz/myfirstapp/Maths.java
@@ -0,0 +1,32 @@
+package com.judydimpoz.myfirstapp;
+/**
+ * Created by STDNT on 2016/02/06.
+ */
+public class Maths {
+
+
+    public static void main(String args[]){
+        //call a method by a method name
+        addition();
+        //call a method by a method name
+        subtraction();
+    }
+    //declare a variable
+}
+    
+//addition method
+public static void addition(){
+    int x = 2;
+    int y = 4;
+    int answer = x + y;
+    System.out.println(answer);
+
+}
+    //subtraction method
+    int x = 6;
+    int y = 2;
+    int answer = x- y;
+    System.out.println(answer);
+
+}
+
---
+++ app/src/main/java/com/judydimpoz/myfirstapp/TestClass.java
@@ -0,0 +1,28 @@
+package com.judydimpoz.myfirstapp;
+
+/**
+ * Created by STDNT on 2016/02/06.
+ */
+public class TestClass {
+
+    public static void main (String args []){
+        //declare a variable
+        int age;
+        double shoesSize;
+        char letter;
+        String firstName;
+
+        //initialise the variable
+        age=10;
+        shoesSize =5.5;
+        letter = 'A';
+        firstName = "Young Programmer";
+        System.out.println(age);
+        System.out.println(shoesSize);
+        System.out.println(letter);
+        System.out.println(firstName);
+
+
+    }
+
+}
